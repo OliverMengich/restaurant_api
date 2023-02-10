c3e2226da43d669985d12e3d73b3691b49eefea1
OliverMengich <kemeioliver2000@gmail.com>
2023-02-11 01:59:12 +0300

    Removes babel

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,20 +1,24 @@
 import chaiHTTP from 'chai-http'
 import chai from 'chai';
 import app from '../../app.js';
+import config from '../../src/config/config.js';
+
 chai.should();
 chai.use(chaiHTTP);
 const token = "";
+console.log('PORT is: ', parseInt(config.PORT));
 describe('Integration Testing(Restaurant API)',()=>{
     describe('Get Customers',()=>{
-        it("should login an admin",(done)=>{
-            chai.request(`http://localhost:${process.env.PORT}`).post('/api/auth')
+        it("Fails login an admin",(done)=>{
+            chai.request(`http://localhost:${config.PORT}`).post('/api/auth')
             .send({
                 email:"admin@gmail.com",
                 password:"password",
             }).end((err,res)=>{
-                // console.log(res);
+                // console.log('Error is: ',err);
+                // console.log('Res is: ',res.body);
                 res.should.have.status(401);
-                // res.body.should.be.a('object');
+                res.body.should.be.a('object');
                 // res.body.should.have.property('token');
                 // token = res.body.token;
                 done();
@@ -22,51 +26,57 @@ describe('Integration Testing(Restaurant API)',()=>{
             )
         })
         it('it should get only customer',(done)=>{
-            chai.request(app).get('/api/customers')
+            chai.request(`http://localhost:${config.PORT}`)
+            .get("/api/customers")
             // .auth("Bearer",token.toString())
-            .end((err,res)=>{
+            .end((err, res) => {
                 console.log(res.status);
                 res.should.have.status(200);
-                res.body.customers.should.be.a('array');
-                done()
-            })
+                res.body.customers.should.be.a("array");
+                done();
+            });
         })
     });
     describe('Fails on ading admin without AdminLogin', () => {
         it('Fails upon adding admin without authentification',(done)=>{
-            chai.request(app).post('/api/customers')
-            .set('Content-Type', 'application/json')
-            .send({
-                firstName: "admin",
-                lastName: "admin",
-                birthday: new Date().toISOString(),
-                email: "email@admin.com",
-                phone:"0712345678",
-                userType:"ADMIN"
-            }).auth("Bearer","")
-            .end((err,res)=>{
-                res.should.have.status(401);
-                // res.body.should.be({error:"Not Authenticated"})
-                done();
-            })
+            chai.request(`http://localhost:${config.PORT}`)
+                .post("/api/customers")
+                .set("Content-Type", "application/json")
+                .send({
+                    firstName: "admin",
+                    lastName: "admin",
+                    birthday: new Date().toISOString(),
+                    email: "email@admin.com",
+                    phone: "0712345678",
+                    userType: "ADMIN",
+                })
+                .auth("Bearer", "")
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    // res.body.should.be({error:"Not Authenticated"})
+                    done();
+                });
         }) 
     })
     describe('Passes Upon adding an administrator when logged in', () => {
         it('',(done)=>{
-            chai.request(app).post('/api/customers')
-            .send({
-                firstName: "Admin",
-                lastName:"Admin",
-                email:"admin1@gmail.com",
-                password:"password",
-                userType:"ADMIN",
-                birthday:new Date().toISOString().toString()
-            }).auth("Bearer","")
-            .end((err,res)=>{
-                chai.expect(res).to.have.status(401);
-                // res.body.should.be.a('object');
-                res.body.should.have.property('error')
-            })
+            chai.request(`http://localhost:${config.PORT}`)
+                .post("/api/customers")
+                .send({
+                    firstName: "Admin",
+                    lastName: "Admin",
+                    email: "admin1@gmail.com",
+                    password: "password",
+                    userType: "ADMIN",
+                    birthday: new Date().toISOString().toString(),
+                    phone: "1234567895"
+                })
+                .auth("Bearer", "")
+                .end((err, res) => {
+                    chai.expect(res).to.have.status(401);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property("error");
+                });
             done();
         })
     })
